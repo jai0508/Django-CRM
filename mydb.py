79a75900867321789b3ddea766ccc1e7e22dcbb9
jai0508 <jaisuryan.jaswal.05@gmail.com>
2024-10-24 16:30:46 +0530

    First release of Hello World!

---
+++ mydb.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+    database= 'elderco',
+	password = 'password123',
+    auth_plugin='mysql_native_password'
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+#cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
+
+
